950fce801b27c50cfd2b6eb13bf56f71a6827c9d
Bulation <insanbulat@gmail.com>
2021-09-08 22:56:44 +0300

    refactor: change adaptive menu positioning

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
-const sideMenu = document.querySelector('.side-menu');
-const sideMenuItems = document.querySelectorAll('.side-menu li')
+const sideMenu = document.querySelector('.navigation');
+const sideMenuItems = document.querySelectorAll(".navigation-list__item");
 const containers = document.querySelectorAll('.container')
 const mark = document.querySelector('mark')
 const closeMenu = document.querySelector('.close')
 closeMenu.addEventListener('click', toggleHamburger);
 hamburger.addEventListener('click', toggleHamburger);
+
 for (let item of sideMenuItems)
   item.addEventListener("click", toggleHamburger);
+
 function toggleHamburger() {
+  closeMenu.classList.toggle("hidden");
   sideMenu.classList.toggle("transform");
   for (let container of containers)
     container.classList.toggle('backFogging');
