34daf29fb608e5cfc35f3adcddd7ef9cb5ecd1c8
Bulation <insanbulat@gmail.com>
2021-09-09 23:40:00 +0300

    feat: improve script by clicking on body

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ const sideMenuItems = document.querySelectorAll(".navigation-list__item");
 const containers = document.querySelectorAll('.container')
 const mark = document.querySelector('mark')
 const closeMenu = document.querySelector('.close')
+
 closeMenu.addEventListener('click', toggleHamburger);
 hamburger.addEventListener('click', toggleHamburger);
 
-for (let item of sideMenuItems)
-  item.addEventListener("click", toggleHamburger);
+for (let container of containers) {
+  container.addEventListener("click", hideMenuByClickOnBody);
+}
 
-function toggleHamburger() {
+function toggleHamburger(e) {
   closeMenu.classList.toggle("hidden");
   sideMenu.classList.toggle("transform");
-  for (let container of containers)
-    container.classList.toggle('backFogging');
+  for (let container of containers) {
+    container.classList.toggle("backFogging");
+  }
+  for (let item of sideMenuItems)
+    item.addEventListener("click", toggleHamburger);
   mark.classList.toggle("black");
+}
+
+function hideMenuByClickOnBody(e) {
+  if (!e.target.closest(".navigation") && !e.target.closest(".hamburger") && !closeMenu.matches(".hidden"))
+    toggleHamburger(e);
 }
